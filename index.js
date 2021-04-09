b1650029de3df59999357199084bd3f9408c9d58
Sandeep Anumalla <42109850+sandeepanumalla@users.noreply.github.com>
2021-04-09 11:26:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ console.log("running production true");
   app.use(express.static('client/build'));
   
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
   })
 
 }
