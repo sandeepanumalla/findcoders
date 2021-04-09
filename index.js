452450f4b6443ba0dfc2f12257c7dbe84b40fcd4
Sandeep Anumalla <42109850+sandeepanumalla@users.noreply.github.com>
2021-04-09 11:02:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-let express = require('express');
-let mongoose = require("mongoose");
-const {Users} = require('./models/user')
-const bodyParser = require('body-parser');
-const cors = require("cors");
-
-mongoose.connect(`mongodb+srv://Sandeep:Sandeep99@cluster0.7qa74.mongodb.net/test`,{useNewUrlParser: true,useUnifiedTopology: true})
-.then((data)=>{console.log("successfully connected to db")})
-.catch((err)=>{console.log("error connecting to db",err)})
-
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+require('dotenv/config')
+const {Users, userSchema} = require('./models/user');
+const cors = require('cors')
+const userRoutes = require('./routes/user');
+ const authRoutes = require('./routes/authUser')
+const  mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const path = require('path');
+const cookieParser = require('cookie-parser')
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-
-let app =express();
-const userRoute = require('./routes/authRoutes');
-const profileRoute = require('./routes/Profiles');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+mongoose.connect("mongodb://localhost:27017/userlms")
+.then(()=>    console.log("succesfully connected to mongodb. "))
+.catch((err)=>console.log("error connecting",err))
+console.log("mongoport",process.env.MONGODB_URI)
+const port = process.env.PORT || 8000;
 app.use(cors());
-app.use('/api/users',userRoute);
-app.use('/api/profile',profileRoute);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+/* app.use(express.urlenstarcoded({extended:false})) */
 
-app.get("/",(req,res)=>{
-    res.send("hello world");
-})
-/* app.use('/login', (req, res) => {
-    res.send({
-      token: 'test123'
-    });
-  }); */
-const port = process.env.PORT || 8080
-app.listen(port,()=>{
-    console.log(`App is listening to port ${port}`);
-})
-/* server.listen(8080,()=>{
-    console.log("server is listening ro port 8k");
-}); */
+
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+
+app.use('/api/users', authRoutes);
+app.use('/api/users', userRoutes);
+
+
+if(process.env.NODE_ENV === 'production')
+{
+    console.log("true");
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))  //relative path
+    })
+}
+
+
+console.log("secret msg => ",process.env.SECRET)
+app.listen(port,()=> console.log(`Listenning on port ${port}`))
+
